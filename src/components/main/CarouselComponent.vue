32165aa45eac5757dc0e6e00197dd4c5098fbbaa
Bilobra <bra.carrozzo@gmail.com>
2023-02-18 18:46:00 +0100

    fix carousel/ img in assets

--- src/components/main/CarouselComponent.vue
+++ src/components/main/CarouselComponent.vue
@@ -1,13 +1,53 @@
 <template>
-    <div>
+    <div class="carousel">
+        <slot></slot>
 
+        <button  @click="next" class="next">Next</button>
+        <button @click="prev" class="prev">Prev</button>
     </div>
 </template>
 
 <script>
 export default {
     name: 'CarouselComponent',
+    methods:{
+        next(){
+            this.$emit('next')
+        },
+        prev(){
+            this.$emit('prev')
+
+        }
+    }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.carousel {
+    position: relative;
+    width: 80%;
+    height: 400px;
+    overflow: hidden;
+
+}
+
+button {
+    position: absolute;
+    height: 40px;
+    width: 50px;
+    top: calc(50% - 20px);
+    background-color: rgba(0,0,0,0.8);
+    border: none;
+    color:white;
+}
+button:focus, button:hover{
+    outline: none;
+    cursor: pointer;
+}
+.next{
+    right: 0;
+}
+.prev{
+    left: 0;
+}
+</style>
